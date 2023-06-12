9780b192d57ddb96370a53475ee46d66f6c00bbc
Wandati <Petersimon801@gmail.com>
2023-06-12 19:27:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+console.log(multiply)
+
+const Number= (Math.random() * 10) + 1;
+console.log(Number)
+let num3 = 22;
+let num4 = 9;
+const mod = num3 % num4;
+
+console.log(mod)
+let numbers = (5,10,15,20);
+const max =(Math.max(numbers));
+console.log(max)
